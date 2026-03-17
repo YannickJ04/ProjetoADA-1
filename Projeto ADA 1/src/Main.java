36333c01203653212bdb83b9426d21784d210a17
YannickJ04 <157326495+YannickJ04@users.noreply.github.com>
2026-03-17 22:47:14 +0000

    yuu

--- Projeto ADA 1/src/Main.java
+++ Projeto ADA 1/src/Main.java
@@ -1,22 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Criando um grafo
-        Grafo grafo = new Grafo();
 
-        // Adicionando vértices
-        grafo.adicionarVertice("A");
-        grafo.adicionarVertice("B");
-        grafo.adicionarVertice("C");
-        grafo.adicionarVertice("D");
-
-        // Adicionando arestas
-        grafo.adicionarAresta("A", "B");
-        grafo.adicionarAresta("A", "C");
-        grafo.adicionarAresta("B", "D");
-        grafo.adicionarAresta("C", "D");
-
-        // Exibindo o grafo
-        System.out.println(grafo);
     }
 }
